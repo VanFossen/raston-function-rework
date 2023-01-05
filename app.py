eaa41b23472a5949d2024f9d75bc048bbee07c56
Nicholas VanFossen <nvanfossen@gmail.com>
2023-01-05 14:06:45 -0500

    update debug statements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #%%
 import json
 
+import matplotlib.pyplot as plt
 import numpy as np
 from radis import SerialSlabs, Spectrum, calc_spectrum
 from radis.spectrum.operations import add_array
@@ -17,12 +18,15 @@ from blackbody_window_detector import (
 )
 from helper import __calc_wstep, __param_check
 
-DEBUG = True
+DEBUG = False
 
 
-def __log(string):
+def __log(string, spectrum=False, title=False):
     if DEBUG:
         print(string)
+        if spectrum is not False:
+            spectrum.plot("transmittance_noslit")
+            plt.title(title)
 
 
 def main():
@@ -59,9 +63,7 @@ def main():
             verbose=False,
             warnings={"AccuracyError": "ignore"},
         )
-
-        __log("plotting result of 'calc_spectrum'...")
-        s.plot("transmittance_noslit", wunit="cm-1")
+        __log("plotting result of 'calc_spectrum'...", s, "raw spectrum")
     except Exception as e:
         print(f"ERROR: {e}")
         return False
@@ -83,8 +85,7 @@ def main():
         units={"transmittance_noslit": ""},
         name="sPlanck",
     )
-    __log("plotting result of 'spec_sPlanck'...")
-    spec_sPlanck.plot("transmittance_noslit")
+    __log("plotting result of 'spec_sPlanck'...", spec_sPlanck, "raw sPlanck")
 
     __log("performing 'Spectrum()' for AR_ZnSe...")
     spec_AR_ZnSe = Spectrum(
@@ -93,8 +94,7 @@ def main():
         units={"transmittance_noslit": ""},
         name="AR_ZnSe",
     )
-    __log("plotting result of 'spec_AR_ZnSe'...")
-    spec_AR_ZnSe.plot("transmittance_noslit")
+    __log("plotting result of 'spec_AR_ZnSe'...", spec_AR_ZnSe, "raw AR_ZnSe")
 
     __log("performing 'Spectrum()' for AR_CaF2...")
     spec_AR_CaF2 = Spectrum(
@@ -103,8 +103,7 @@ def main():
         units={"transmittance_noslit": ""},
         name="AR_CaF2",
     )
-    __log("plotting result of 'spec_AR_CaF2'...")
-    spec_AR_CaF2.plot("transmittance_noslit")
+    __log("plotting result of 'spec_AR_CaF2'...", spec_AR_CaF2, "raw AR_CaF2")
 
     __log("performing 'Spectrum()' for CaF2...")
     spec_CaF2 = Spectrum(
@@ -113,8 +112,7 @@ def main():
         units={"transmittance_noslit": ""},
         name="CaF2",
     )
-    __log("plotting result of 'spec_CaF2'...")
-    spec_CaF2.plot("transmittance_noslit")
+    __log("plotting result of 'spec_CaF2'...", spec_CaF2, "raw CaF2")
 
     __log("performing 'Spectrum()' for ZnSe...")
     spec_ZnSe = Spectrum(
@@ -123,8 +121,7 @@ def main():
         units={"transmittance_noslit": ""},
         name="ZnSe",
     )
-    __log("plotting result of 'spec_ZnSe'...")
-    spec_ZnSe.plot("transmittance_noslit")
+    __log("plotting result of 'spec_ZnSe'...", spec_ZnSe, "raw ZnSe")
 
     __log("performing 'Spectrum()' for sapphire...")
     spec_sapphire = Spectrum(
@@ -133,8 +130,7 @@ def main():
         units={"transmittance_noslit": ""},
         name="sapphire",
     )
-    __log("plotting result of 'spec_sapphire'...")
-    spec_sapphire.plot("transmittance_noslit")
+    __log("plotting result of 'spec_sapphire'...", spec_sapphire, "raw sapphire")
 
     __log("performing 'Spectrum()' for MCT...")
     spec_MCT = Spectrum(
@@ -143,8 +139,7 @@ def main():
         units={"transmittance_noslit": ""},
         name="MCT",
     )
-    __log("plotting result of 'spec_MCT'...")
-    spec_MCT.plot("transmittance_noslit")
+    __log("plotting result of 'spec_MCT'...", spec_MCT, "raw MCT")
 
     __log("performing 'Spectrum()' for InSb...")
     spec_InSb = Spectrum(
@@ -153,51 +148,62 @@ def main():
         units={"transmittance_noslit": ""},
         name="InSb",
     )
-    __log("plotting result of 'spec_InSb'...")
-    spec_InSb.plot("transmittance_noslit")
+    __log("plotting result of 'spec_InSb'...", spec_InSb, "raw InSb")
 
     # ----- b.) blackbody spectrum of source -----
     # SerialSlabs() multiplies the transmittance values (y-values) of the provided spectrum, s, with the corresponding transmittance values of the spec_sPlanck spectrum
     #   https://radis.readthedocs.io/en/latest/source/radis.los.slabs.html#radis.los.slabs.SerialSlabs
     __log("combining spectrum with blackbody ('sPlanck()')...")
     spectrum = SerialSlabs(s, spec_sPlanck)
-    spectrum.plot("transmittance_noslit")
+    __log("plotting new combined spectrum...", spectrum, "raw + blackbody")
 
     # This loop simulates scans and runs as many times as the user indicated in "Number of Scans"
     for x in range(data["numScan"]):
         # ----- c.) transmission spectrum of windows/beamsplitter -----
         # ----- c.1) Beamsplitter -----
         __log("combining spectrum with beamsplitter ('AR_ZnSe()' or 'AR_CaF2()')...")
-        if data["beamsplitter"] == "AR_ZnSe":
-            spectrum = SerialSlabs(spectrum, spec_AR_ZnSe)
-        elif data["beamsplitter"] == "AR_CaF2":
-            spectrum = SerialSlabs(spectrum, spec_AR_CaF2)
-        __log("plotting beamsplitter result...")
-        spectrum.plot("transmittance_noslit")
+        match data["beamsplitter"]:
+            case "AR_ZnSe":
+                spectrum = SerialSlabs(spectrum, spec_AR_ZnSe)
+            case "AR_CaF2":
+                spectrum = SerialSlabs(spectrum, spec_AR_CaF2)
+        __log(
+            "plotting beamsplitter result...",
+            spectrum,
+            "raw + blackbody + beamsplitter",
+        )
 
         # ----- c.2) cell windows -----
         __log("combining spectrum with cell windows ('CaF2()' or 'ZnSe()')...")
-        if data["cellWindow"] == "CaF2":
-            spectrum = SerialSlabs(spectrum, spec_CaF2)
-            spectrum = SerialSlabs(spectrum, spec_CaF2)
-        elif data["cellWindow"] == "ZnSe":
-            spectrum = SerialSlabs(spectrum, spec_ZnSe)
-            spectrum = SerialSlabs(spectrum, spec_ZnSe)
-        __log("plotting cell windows result...")
-        spectrum.plot("transmittance_noslit")
+        match data["cellWindow"]:
+            case "CaF2":
+                spectrum = SerialSlabs(spectrum, spec_CaF2)
+                spectrum = SerialSlabs(spectrum, spec_CaF2)
+            case "ZnSe":
+                spectrum = SerialSlabs(spectrum, spec_ZnSe)
+                spectrum = SerialSlabs(spectrum, spec_ZnSe)
+        __log(
+            "plotting cell windows result...",
+            spectrum,
+            "raw + blackbody + beamsplitter + cell window",
+        )
 
         # ----- d.) detector response spectrum -----
         __log(
             "combining spectrum with detector ('ZnSe()' & 'MCT()' or 'sapphire()' & 'InSb()')..."
         )
-        if data["detector"] == "MCT":
-            spectrum = SerialSlabs(spectrum, spec_ZnSe)
-            spectrum = SerialSlabs(spectrum, spec_MCT)
-        elif data["detector"] == "InSb":
-            spectrum = SerialSlabs(spectrum, spec_sapphire)
-            spectrum = SerialSlabs(spectrum, spec_InSb)
-        __log("plotting detector result...")
-        spectrum.plot("transmittance_noslit")
+        match data["detector"]:
+            case "MCT":
+                spectrum = SerialSlabs(spectrum, spec_ZnSe)
+                spectrum = SerialSlabs(spectrum, spec_MCT)
+            case "InSb":
+                spectrum = SerialSlabs(spectrum, spec_sapphire)
+                spectrum = SerialSlabs(spectrum, spec_InSb)
+        __log(
+            "plotting detector result...",
+            spectrum,
+            "raw + blackbody + beamsplitter + cell window + detector",
+        )
 
         # add random noise to spectrum
         __log("adding noise to spectrum...")
@@ -206,8 +212,11 @@ def main():
             np.random.normal(0, 200000000, len(spectrum.get_wavenumber())),
             var="transmittance_noslit",
         )
-        __log("plotting noise result...")
-        spectrum.plot("transmittance_noslit")
+        __log(
+            "plotting noise result...",
+            spectrum,
+            "raw + blackbody + beamsplitter + cell window + detector + noise",
+        )
 
     # NOTE additional code that I am unsure if needed or needs to be rewritten
     # ------------------------------------------------------------------------
@@ -236,6 +245,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
 # %%
